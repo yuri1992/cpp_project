e86513632d53585a7573441e4238c511b20493ea
yuri1992 <yuri1992@gmail.com>
2016-12-09 16:27:03 +0200

    Handling menus and key press

--- TheGame.cpp
+++ TheGame.cpp
@@ -23,16 +23,106 @@ void TheGame::init()
 	snakes[1]->printSnake();
 }
 
+void TheGame::printScreen()
+{
+	if (status == Game::STARTED || status == Game::SHOW_QUICK_MENU)
+	{
+		showQuickMenu();
+	}
+	else if (status == Game::PAUSE || status == Game::SHOW_FULL_MENU)
+	{
+		showFullMenu();
+	}
+}
+
 void TheGame::run()
 {
-	init();
-	// Todo: Should be added handling of other keys press like (ESC, Menu buttons)
-	char key = 0;
+	printScreen();
 	do
 	{
+		if (status == Game::STARTED ||
+			status == Game::SHOW_QUICK_MENU ||
+			status == Game::PAUSE ||
+			status == Game::SHOW_FULL_MENU)
+		{
+			_handleMenuKeyPress();
+		}
+		else
+		{
+			_handleGameKeyPress();
+		}
+	}
+	while (true);
+}
+
+
+void TheGame::_handleMenuKeyPress()
+{
+	if (_kbhit())
+	{
+		char key = 0;
+		key = _getch();
+		if (status == Game::STARTED || status == Game::SHOW_QUICK_MENU)
+		{
+			// handling Quick Menu keys press
+			switch (key)
+			{
+			case NUM_1:
+				_showInformation();
+				break;
+			case NUM_2:
+				_start();
+				break;
+			case NUM_9:
+				_exit();
+				break;
+			default: break;
+			}
+		}
+		else if (status == Game::PAUSE || status == Game::SHOW_FULL_MENU)
+		{
+			// handling Full Menu keys press
+			switch (key)
+			{
+			case NUM_1:
+				_exit();
+				break;
+			case NUM_2:
+				_showFullMenu();
+				break;
+			case NUM_3:
+				_continue();
+				break;
+			case NUM_4:
+				_restartGame();
+				break;
+			case NUM_5:
+				_nextGame();
+				break;
+			case NUM_6:
+				_newGame();
+				break;
+			default: break;
+			}
+		}
+	}
+}
+
+void TheGame::_handleGameKeyPress()
+{
+	if (status == Game::RUNNING)
+	{
+		char key = 0;
 		if (_kbhit())
 		{
 			key = _getch();
+
+			if (key == ESC)
+			{
+				_pause();
+				return;
+			}
+
 			snakes[0]->handleKey(key);
 			snakes[1]->handleKey(key);
 		}
@@ -41,20 +131,59 @@ void TheGame::run()
 		snakes[1]->move();
 		Sleep(400);
 	}
-	while (key != ESC);
+}
+
+void TheGame::showInformation()
+{
+	// Todo ITAY: implement Information game screen
+	// 1. erase all screen or print in the top first line.
+	// I do not understood the insrcution for the quick menu, try to figure out what Amir mean.
+	cout << "Information " << endl;
 }
 
 void TheGame::showQuickMenu()
 {
-	// Todo: implement quick menu print
+	// Todo ITAY: implement quick menu print
 	// 1. erase all screen or print in the top first line.
-
+	// I do not understood the insrcution for the quick menu, try to figure out what Amir mean.
+	cout << "Welcome " << endl;
 }
 
 void TheGame::showFullMenu()
 {
-	// Todo: implement quick menu print
+	// Todo ITAY: implement quick menu print
 	// 1. erase all screen
 	// 2. gotoxy middle screen
 	// 3. print menu.
+	cout << "Full " << endl;
+}
+
+void TheGame::_start()
+{
+	status = Game::RUNNING;
+	init();
+}
+
+void TheGame::_continue()
+{
+	// TODO ITAY : we should print once again the board
+	status = Game::RUNNING;
+}
+
+void TheGame::_restartGame()
+{
+	// TODO ITAY : we should be able to restart the game to same position of the initial mission
+	status = Game::RUNNING;
+}
+
+void TheGame::_nextGame()
+{
+	// TODO ITAY: we should be able to break the current mission and start new mission.
+	status = Game::RUNNING;
+}
+
+void TheGame::_newGame()
+{
+	// TODO ITAY: we should be able restart all the game to initial point.
+	status = Game::RUNNING;
 }
--- TheGame.h
+++ TheGame.h
@@ -13,22 +13,28 @@ class TheGame
 	enum
 	{
 		ESC = 27,
-		NUM_1 = 1,
-		NUM_2 = 1,
-		NUM_3 = 1,
-		NUM_4 = 1,
-		NUM_5 = 1,
-		NUM_6 = 1,
-		NUM_7 = 7,
+		NUM_1 = '1',
+		NUM_2 = '2',
+		NUM_3 = '3',
+		NUM_4 = '4',
+		NUM_5 = '5',
+		NUM_6 = '6',
+		NUM_7 = '7',
+		NUM_8 = '8',
+		NUM_9 = '9'
 	};
 
 	enum Game
 	{
 		PAUSE = 0,
 		RUNNING = 1,
+		STARTED = 2,
+		SHOW_QUICK_MENU = 3,
+		SHOW_FULL_MENU = 4,
+		SHOW_INFORMATION = 5,
 	};
 
-	Game status = PAUSE;
+	Game status = STARTED;
 	Snake** snakes;
 	BoardManager* boardManager;
 	MissionBase mission;
@@ -36,17 +42,30 @@ class TheGame
 public:
 	TheGame(const char* board[ROWS]);
 	void init();
+
+	void printScreen();
 	void run();
 	void showQuickMenu();
 	void showFullMenu();
 	bool isSolveMission(const Point& p) { return mission.isSolved(); }
+
 	void setBoardManager(const char* board[ROWS]) { boardManager->setBoard(board); };
 
 private:
-	void _pause();
+	void _exit() { exit(0); }
+
+	void _pause() { status = Game::PAUSE; printScreen(); }
+	void _showInformation() { status = Game::SHOW_INFORMATION;  printScreen(); }
+	void _showFullMenu() { status = Game::SHOW_FULL_MENU; printScreen(); }
+
+	void _start();
 	void _continue();
 	void _restartGame();
-
+	void _nextGame();
+	void _newGame();
+	void _handleMenuKeyPress();
+	void _handleGameKeyPress();
+	void showInformation();
 };
 
 #endif
